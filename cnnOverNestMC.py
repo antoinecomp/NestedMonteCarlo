64b64d076faf978c5e8fe654229f878fa0b85ba7
Your Name <antoine.compagnie@gmail.com>
2018-03-13 13:24:53 +0100

    cpp updated and working

--- cnnOverNestMC.py
+++ cnnOverNestMC.py
@@ -18,28 +18,28 @@ from keras.layers import Dense, Embedding, GlobalMaxPooling1D
 from keras.preprocessing.text import Tokenizer
 from keras.optimizers import Adam
 
-import time, thread
+import time
 
 class Board:
 
-    """Class defining a Board characterized by :
-    - an array"""
+	"""Class defining a Board characterized by :
+	- an array"""
 
-    def __init__(self):
+	def __init__(self):
 
-        """Class Board constructor, define an array initialized with 0s"""
-		board = [8][8]
-		for i in range (0,8):
-			for j in range (0,8):
+		"""Class Board constructor, define an array initialized with 0s"""
+		board = [7][7]
+		for i in range (0,7):
+			for j in range (0,7):
 				board[i][j] = 0
 
-int listPlayable(currentPlayer, board):
-    """Method defining how many possibilites one player have"""
+def listPlayable(currentPlayer, board):
+	"""Method defining how many possibilites one player have"""
 	# what vertical plays may the vertical player have
-	if(currentPlayer==0):
-		for i in range (0,8):
+	if(currentPlayer==1):
+		for i in range (0,7):
 			# we shouldn't go above the last lines when testing the cases emptiness so we go until 7 for j
-			for j in range (0,7):
+			for j in range (0,6):
 				# we check if one position and the one below are empty
 				if(board[i][j] == 0 and board[i][j+1] == 0):
 					playable = {i,j}
@@ -48,52 +48,83 @@ int listPlayable(currentPlayer, board):
 					print(currentPlayer + "possibilies" + l.length)
 					return l
 	# what horizontal plays may the horizontal player have
-	else(currentPlayer==1):
+	else:
 		# we shouldn't go above the last column when testing the cases emptiness so we go until 7 for i
-		for i in range (0,8):
-			for j in range (0,8):
+		for i in range (0,6):
+			for j in range (0,7):
 				# we check if one position and the one below are empty
-				if(board[i][j] = 0 board[i+1][j] == 0):
+				if(board[i][j] == 0 and board[i+1][j] == 0):
 					playable = {i,j}
 					# we store his possible moves in his own list
 					l.append(playable)
 					print(currentPlayer + "possibilies" + l.length)
 					return l
 
+
+def MT_Explorate(board, playerNumber, move):
+	PlayPosition(move, playerNumber, board)
+	return MT_Simulate(board, (playerNumber+1)%2) # ???
+
+def PlayPosition(boardPosition, playerNumber, board):
+	if(playerNumber == 1):
+		board[boardPosition.x][boardPosition.y] = '1'
+		board[boardPosition.x+1][boardPosition.y] = '1'
+	else:
+		board[boardPositionboardPosition.x][bp.y] = '2'
+		board[boardPosition.x][boardPosition.y+1] = '2'
+
+################ Danger Zone ##################
 class UCTResult:
-    """I don't understand what this Class defines. It has :
-    - number of victory
-	- number of playouts
+	"""I don't understand what this Class defines. It has :
+	- a number of victory
+	- a number of playouts
 	- a board position, I don't know what it is
 	- a vector of UCTResults, I don't know what it is"""
 
-    def __init__(self):
+	def __init__(self):
 
-        """Class UCTResult constructor, define an array initialized with 0s"""
+		"""Class UCTResult constructor, define an array initialized with 0s"""
+		victory = 0
+		playout = 0
+		play = BoardPosition()
+		# vector of UCTResults, I don't know what it corresponds in
+
+	def __init__(self, position):
+
+		"""Class UCTResult constructor, define an array initialized with 0s"""
 		victory = 0
 		playout = 0
 		play = BoardPosition()
 		# vector of UCTResults, I don't know what it corresponds in
 
 class BoardPosition:
-    """Class defining the board positions, USELESS ?:
-    - """
+	"""Class defining the board positions, USELESS ?:
+	- """
 
-    def __init__(self):
-        """Class BoardPosition constructor, define an array initialized with 0s"""
+	def __init__(self):
+		"""Class BoardPosition constructor"""
+#################################################
 
 
 
 def MonteCarlo():
 	# player 0 plays vertically, player 1 horizontally
-	int currentPlayer = 0
+	currentPlayer = 1
 	board = Board()
-	pos = listPlayable(0,board)
+	playableMoves = listPlayable(1,board)
+	print(playableMoves)
+	for move in playableMoves: # shouldn't we .pop() them when they are no more useful ?
+		res = UCTResult(move)
+		victory = MT_Explorate(board, currentPlayer, move)
+		
 	# We gona make 100 random game to learn with Monte Carlo
 	# then he plays with his knowledge, the other play random
 
-std::vector<BoardPosition> pos = ListPlayable(currentPlayer, board)
+#std::vector<BoardPosition> pos = ListPlayable(currentPlayer, board)
 
 
 if __name__ == "__main__":
-    MonteCarlo()
+	domineering = pd.read_csv("~/NestedMonteCarlo/domineering.csv").fillna("sterby")
+	for i from 0 to 1000:
+		MonteCarlo()
+    return 0
--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <fstream>
+#include <cstdlib>
 
 struct Board
 {
-	char board[5][5];
+	char board[8][8];
 	Board() {}
 	Board(const Board& b) 
 	{
-		for(int i=0; i < 5; ++i)
+		for(int i=0; i < 8; ++i)
 		{
-			for(int j=0; j < 5; ++j)
+			for(int j=0; j < 8; ++j)
 			{
 				board[i][j] = b.board[i][j];
 			}
@@ -22,6 +25,7 @@ struct BoardPosition
 {
 	int x;
 	int y;
+	BoardPosition() {}
 	BoardPosition(int _x, int _y) : x(_x), y(_y) {}
 };
 
@@ -32,22 +36,23 @@ struct UCTResult
 	BoardPosition play;
 	std::vector<UCTResult*> next;
 	UCTResult* previous=nullptr;
-	float Evaluate() 
-	{
-		
-	}
+
 	UCTResult() {}
 	UCTResult(BoardPosition pos) : play(pos) { }
-	~UCTResult() {}
+	~UCTResult() { 
+		for (unsigned int i = 0; i < next.size(); ++i)
+			delete next[i];
+		next.clear();
+	}
 };
 
 //char board[5][5];
 
 void ClearBoard(Board& board)
 {
-	for(int i=0; i < 5; ++i)
+	for(int i=0; i < 8; ++i)
 	{
-		for(int j=0; j < 5; ++j)
+		for(int j=0; j < 8; ++j)
 		{
 			board.board[i][j] = ' ';
 		}
@@ -56,19 +61,19 @@ void ClearBoard(Board& board)
 
 void DrawBoard(Board& board)
 {
-	for(int i=0; i < 5; ++i)
+	for(int i=0; i < 8; ++i)
 	{
 		std::cout << "----";
 	}
 	std::cout << '-' << std::endl;
-	for(int i=0; i < 5; ++i)
+	for(int i=0; i < 8; ++i)
 	{
-		for(int j=0; j < 5; ++j)
+		for(int j=0; j < 8; ++j)
 		{
 			std::cout << "| " << board.board[i][j] << " ";
 		}
 		std::cout << '|' << std::endl;
-		for(int j=0; j < 5; ++j)
+		for(int j=0; j < 8; ++j)
 		{
 			std::cout << "----";
 		}
@@ -86,13 +91,13 @@ bool CanPlay(int playerNumber, int col, int row, Board& board)
 	if(playerNumber == 0)
 	{
 
-		if(row < 1 || col > 4)
+		if(row < 1 || col > 7)
 			return false;
 
 		if(board.board[col][row -1] != ' ')
 			return false;
 	}else{
-		if(col > 3 || row > 4)
+		if(col > 6 || row > 7)
 			return false;
 		if(board.board[col+1][row] != ' ')
 			return false;
@@ -100,20 +105,24 @@ bool CanPlay(int playerNumber, int col, int row, Board& board)
 	return true;
 }
 
-std::vector<BoardPosition> ListPlayable(int playerNumber, Board& board)
+int ListPlayable(int playerNumber, Board& board, BoardPosition* positions)
 {
-	std::vector<BoardPosition> positions;
-	for(int i=0; i < 5; ++i)
+	//std::vector<BoardPosition> positions;
+	int count = 0;
+	for(int i=0; i < 8; ++i)
 	{
-		for(int j=0; j < 5; ++j)
+		for(int j=0; j < 8; ++j)
 		{
 			if(CanPlay(playerNumber, i , j, board))
 			{
-				positions.push_back(BoardPosition(i,j));
+				positions[count] = BoardPosition(i, j);
+				//positions.push_back(BoardPosition(i,j));
+				++count;
 			}
 		}
 	}
-	return positions;
+	return count;
+	//return positions;
 }
 
 void PlayPosition(BoardPosition bp, int playerNumber, Board& board)
@@ -134,18 +143,23 @@ bool SimulateGame(int startingPlayer, Board& board)
 	srand(time(NULL));
 	int i=startingPlayer;
 	bool end = false;
+	int size = 0;
+	//MAx board position possible 8 * 8 = 64 
+	BoardPosition* pos = new BoardPosition[80];
 	while(!end)
 	{
-		std::vector<BoardPosition> pos = ListPlayable(i%2, board);
-		if(pos.size() == 0)
+		//std::vector<BoardPosition> pos = ListPlayable(i%2, board);
+		size = ListPlayable(i % 2, board, pos);
+		if(size == 0)
 			end = true;
 		else
 		{
-			BoardPosition bp = pos[rand() % pos.size()];
+			BoardPosition bp = pos[rand() % size];
 			PlayPosition(bp, i, board);
 			++i;
 		}
 	}
+	delete pos;
 
 	//si i%2 est celui qui a perdu
 	//donc startingPlayer perd si i%2 == startingPlayer
@@ -173,40 +187,92 @@ int MT_Explorate(Board board, int playerNumber, BoardPosition pos )
 	return MT_Simulate(board, (playerNumber+1)%2);
 }
 
+std::ofstream csvFile;
+
+void WriteRow(Board& board, BoardPosition selectedMove, int playerNumber)
+{
+	for (int i = 0; i < 8; ++i)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			csvFile << (board.board[i][j] == ' ' ? "0" : "1") << ",";
+		}
+	}
+
+	for (int i = 0; i < 8; ++i)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			csvFile << (board.board[i][j] == ' ' ? "1" : "0") << ",";
+		}
+	}
+
+	for (int i = 0; i < 8; ++i)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			csvFile << playerNumber << ",";
+		}
+	}
+
+	for (int i = 0; i < 8; ++i)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			if (selectedMove.x == i && selectedMove.y == j)
+			{
+				csvFile << "1";
+			}
+			else {
+				csvFile << "0";
+			}
+
+			if (i != 7 || j != 7)
+				csvFile << ",";
+		}
+	}
+
+	csvFile << std::endl;
+}
+
 void MonteCarlo()
 {
 	int currentPlayer = 0;
 	Board board;
 	ClearBoard(board);
-	std::vector<BoardPosition> pos = ListPlayable(currentPlayer, board);
-	UCTResult *rootNode, *selectedNode, *tmpNode; // what ?
+
+	BoardPosition* pos = new BoardPosition[80];
+	int size = ListPlayable(currentPlayer, board, pos);
+
+	//std::vector<BoardPosition> pos = ListPlayable(currentPlayer, board);
+	UCTResult *rootNode, *selectedNode, *tmpNode;
 	rootNode = new UCTResult();
 	selectedNode = rootNode;
 	//
-	while(pos.size() > 0)
+	while(size > 0)
 	{
-		for(int i=0; i < pos.size(); ++i)
+		for(unsigned int i=0; i < size; ++i)
 		{
 			UCTResult *res = new UCTResult(pos[i]);
 			int victory = MT_Explorate(board, currentPlayer, pos[i]);
 			res->victory = victory;
 			res->playout = 20;
 			res->previous = selectedNode;
-			selectedNode.next.push_back(res);
+			selectedNode->next.push_back(res);
 		}
 		//Max
-		float max = FLT_MAX, tmp;
+		float max = -1.0f, tmp;
 		int maxIndex = -1;
-		for(int i=0; i < selectedNode->next.size(), ++i)
+		for(unsigned int i=0; i < selectedNode->next.size(); ++i)
 		{
-			tmp = selectedNode->next[i].victory / (float)selectedNode->next[i].playout;
+			tmp = selectedNode->next[i]->victory / (float)selectedNode->next[i]->playout;
 			if(tmp > max)
 			{
 				max = tmp;
 				maxIndex = i;
 			}
 		}
-		
+
 		int maxVic = selectedNode->next[maxIndex]->victory;
 		int maxPlayout = selectedNode->next[maxIndex]->playout;
 		tmpNode = selectedNode->next[maxIndex];
@@ -217,17 +283,30 @@ void MonteCarlo()
 			tmpNode->victory += maxVic;
 		}
 
-		PlayPosition(pos[maxIndex], playerNumber , board);
+		tmpNode = selectedNode->next[maxIndex];
+		BoardPosition play = selectedNode->next[maxIndex]->play;
+		selectedNode = tmpNode;
+		PlayPosition(play, currentPlayer, board);
 		
 		currentPlayer = (currentPlayer+1)%2;
-		pos = ListPlayable(currentPlayer, board);
+		size = ListPlayable(currentPlayer, board, pos);
+
+		WriteRow(board, play, currentPlayer);
 	}
+
+	delete pos;
+	delete rootNode, selectedNode, tmpNode;
 }
 
 int main()
 {
-	MonteCarlo();
+	csvFile.open("data.csv");
+	for (int i = 0; i < 1000; ++i)
+	{
+		MonteCarlo();
+	}
 	//getchar();
+	csvFile.close();
 	return 0;
 }
 
